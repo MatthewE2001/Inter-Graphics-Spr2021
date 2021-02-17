24ea9c413b1dfca0fd84fd5478504b66264d4180
matthew.esslie <matthew.esslie@mymail.champlain.edu>
2021-02-16 20:55:20 -0500

    Adding in class changes to better display the shadow pass

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoState/a3_DemoState-unload.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoState/a3_DemoState-unload.c
@@ -89,11 +89,11 @@ void a3demo_unloadFramebuffers(a3_DemoState* demoState)
 {
 	// ****TO-DO:
 	//	-> uncomment framebuffer unloading
-/*	a3_Framebuffer* currentFBO = demoState->framebuffer,
+	a3_Framebuffer* currentFBO = demoState->framebuffer,
 		* const endFBO = currentFBO + demoStateMaxCount_framebuffer;
 
 	while (currentFBO < endFBO)
-		a3framebufferRelease(currentFBO++);*/
+		a3framebufferRelease(currentFBO++);
 }
 
 
@@ -115,8 +115,8 @@ void a3demo_unloadValidate(const a3_DemoState* demoState)
 		* const endTex = currentTex + demoStateMaxCount_texture;
 	// ****TO-DO:
 	//	-> uncomment framebuffer pointers
-/*	const a3_Framebuffer* currentFBO = demoState->framebuffer,
-		* const endFBO = currentFBO + demoStateMaxCount_framebuffer;*/
+	const a3_Framebuffer* currentFBO = demoState->framebuffer,
+		* const endFBO = currentFBO + demoStateMaxCount_framebuffer;
 
 	handle = 0;
 	while (currentBuff < endBuff)
@@ -150,11 +150,11 @@ void a3demo_unloadValidate(const a3_DemoState* demoState)
 
 	// ****TO-DO:
 	//	-> uncomment framebuffer validation
-/*	handle = 0;
+	handle = 0;
 	while (currentFBO < endFBO)
 		handle += (currentFBO++)->handle->handle;
 	if (handle)
-		printf("\n A3 Warning: One or more framebuffers not released.");*/
+		printf("\n A3 Warning: One or more framebuffers not released.");
 }
 
 
