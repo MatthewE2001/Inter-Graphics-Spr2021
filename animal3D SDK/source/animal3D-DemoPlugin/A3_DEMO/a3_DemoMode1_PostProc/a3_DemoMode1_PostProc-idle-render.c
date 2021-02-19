9b0b302854d30443c68098423c1d6183747f2760
matthew.esslie <matthew.esslie@mymail.champlain.edu>
2021-02-19 18:34:21 -0500

    Committing changes that make the models appear on screen

--- animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_PostProc/a3_DemoMode1_PostProc-idle-render.c
+++ animal3D SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_PostProc/a3_DemoMode1_PostProc-idle-render.c
@@ -492,13 +492,13 @@ void a3postproc_render(a3_DemoState const* demoState, a3_DemoMode1_PostProc cons
 	if ((demoState->displayGrid || demoState->displayTangentBases || demoState->displayWireframe) &&
 		renderPass != postproc_renderPassShadow)
 	{
-		// ****TO-DO:
+		// ****DONE:
 		//	-> uncomment overlay FBO activation and configuration
-	/*	// activate scene FBO and clear color; reuse depth
+		// activate scene FBO and clear color; reuse depth
 		currentWriteFBO = demoState->fbo_c16x4_d24s8;
 		a3framebufferActivate(currentWriteFBO);
 		glDisable(GL_STENCIL_TEST);
-		glClear(GL_COLOR_BUFFER_BIT);*/
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		// draw grid aligned to world
 		if (demoState->displayGrid)
